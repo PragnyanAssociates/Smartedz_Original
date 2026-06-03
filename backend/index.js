7bf28d7cb0666e621c493ad93eb6c16be886c682
uday kiran <uky8374488@gmail.com>
2026-06-03 11:22:52 +0530

    studymaterials download fix

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ const io = new Server(server, {
 });
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use('/public', express.static('public'));
 
 const db = mysql.createPool({
     uri: process.env.DATABASE_URL,
